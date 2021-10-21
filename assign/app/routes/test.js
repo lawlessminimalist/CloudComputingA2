b87d73e0136da9b3176dd3c853e52c7656cc37c8
nhavilah <51215537+nhavilah@users.noreply.github.com>
2021-10-21 10:12:56 +1000

    Latest Twitter Attempt

--- assign/app/routes/test.js
+++ assign/app/routes/test.js
@@ -1,39 +1,39 @@
-var Twit = require('twit');
 const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-var io = require('socket.io')(server);
+const logger = require('morgan');
+const axios = require("axios");
+const router = express.Router();
+var Twit = require('twit');
+var io = require('socket.io');
 var fs = require('fs');
 
+router.post('/:number',(req,res)=>{
+    //keep the tweets array as a public item so we can apppend all tweets at to it
+    let tweetArray=[];
+    let tweets = req.body
+    tweets.forEach(tweet => {
+        io.on('connection', function(socket) {
+            T.get('search/tweets', { q: tweet, count: req.params.number }, function(err, data, response) {
+                for (let index = 0; index < data.statuses.length; index++) {
+                    const tweet = data.statuses[index];
+                    var tweetbody = {
+                      'text': tweet.text
+                    }
+                    tweetArray.push(tweetbody);
+                }     
+                io.emit('allTweet',tweetArray)
+            })
+        });
+    });
+    res.send(tweetArray)
+    res.end()
+    
+    var T = new Twit({
+      consumer_key:         'pXoX1DPcMooGje3jECA4ijHDR',
+      consumer_secret:      'FlUJvouA1zrFuERX4ExTjMUFQ4Bfo1kmGpySMjMwoRyhpIK9Rq',
+      access_token:         '1443764547085422597-LxGhqq0aQCkGQ3hf2IqUpeSH8dhTVA',
+      access_token_secret:  'W2I4C9pBRw2MIHMAcJ8kGpN5btWEtB6SqpSibL8YR2w1E',
+      timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    });
+})
 
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-io.on('connection', function(socket) {
-
-    T.get('search/tweets', { q: 'daniel craig', count: 100 }, function(err, data, response) {
-      var tweetArray=[];
-        for (let index = 0; index < data.statuses.length; index++) {
-            const tweet = data.statuses[index];
-            var tweetbody = {
-              'text': tweet.text
-            }
-            tweetArray.push(tweetbody);
-        }     
-        io.emit('allTweet',tweetArray)
-    })
-});
-
-var T = new Twit({
-  consumer_key:         'pXoX1DPcMooGje3jECA4ijHDR',
-  consumer_secret:      'FlUJvouA1zrFuERX4ExTjMUFQ4Bfo1kmGpySMjMwoRyhpIK9Rq',
-  access_token:         '1443764547085422597-LxGhqq0aQCkGQ3hf2IqUpeSH8dhTVA',
-  access_token_secret:  'W2I4C9pBRw2MIHMAcJ8kGpN5btWEtB6SqpSibL8YR2w1E',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
-
-// listen for requests :)
-const listener = server.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+module.exports = router;
